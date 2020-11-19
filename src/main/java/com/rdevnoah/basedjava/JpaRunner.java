b89a70efde2811c3f368f8a20defc76728bc50e4
rdevnoah <zzagam2@gmail.com>
2020-11-19 16:02:24 +0900

    spring data jpa code assign

--- src/main/java/com/rdevnoah/basedjava/JpaRunner.java
+++ src/main/java/com/rdevnoah/basedjava/JpaRunner.java
@@ -1,14 +1,10 @@
 package com.rdevnoah.basedjava;
 
-import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
 
 @Component
 @Transactional
